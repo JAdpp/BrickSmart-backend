3c2a7b76fab8348038ff3c941959b3f9c2ba1ec2
JAdpp <747835493@qq.com>
2024-08-25 16:22:37 +0800

    Add files via upload

--- BackEnd/BackEnd/settings.py
+++ BackEnd/BackEnd/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-m6)z)fp035dw-twp=@b98)^m05(_w9jk^owt&5l8suw9l^*=4z'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -124,5 +124,5 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-MEDIA_URL = '/data/'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR.parent,'')
