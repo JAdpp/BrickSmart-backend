82332edd0a697a28fc676dd17307d8d9f2e25f30
JAdpp <747835493@qq.com>
2024-08-25 16:25:50 +0800

    Add files via upload

--- BackEnd/model3d/urls.py
+++ BackEnd/model3d/urls.py
@@ -11,5 +11,5 @@ urlpatterns = [
 ]
 
 # 在开发环境下添加媒体文件的服务
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+#if settings.DEBUG:
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
